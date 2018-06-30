cf56d9038405c782cdb967133dcb8ad0660325a9
Daniel Keen <keendaniel01@gmail.com>
2018-06-30 12:14:54 +0100

    Create folder for tests

--- App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-// import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-const wrapper = shallow(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
