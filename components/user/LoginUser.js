c82a8348d58ddf38f1af84a53d0c6fb8bead69f5
Daniel Keen <keendaniel01@gmail.com>
2018-07-01 18:42:09 +0100

    User login returns auth token

--- components/user/LoginUser.js
+++ components/user/LoginUser.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { compose, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import UserForm from './UserForm';
 
-export default class LoginUser extends Component {
-  loginUser = () => {
-
-  }
+class LoginUser extends Component {
+  loginUser = async({ email, password }) => {
+    try {
+      const signin = await this.props.signinUser({
+        variables: { email, password }
+      });
+      console.log(signin.data.signinUser.token);
+    } catch(error) {
+      console.log(error);
+    }
+  };
 
   render() {
     return (
       <View>
         <Text>Login</Text>
         <UserForm
-          onSubmit={this.createUser}
+          onSubmit={this.loginUser}
           type="Login"
         />
       </View>
     )
   }
 }
+
+const signinUser = gql `
+  mutation signinUser($email: String!, $password: String!) {
+    signinUser(email: { email: $email, password: $password }) {
+      token
+    }
+  }
+`
+
+export default graphql(signinUser, { name: "signinUser" })(LoginUser);
