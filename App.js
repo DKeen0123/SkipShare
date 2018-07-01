e66574187143aa1e67cb35f7132160da9085997a
Daniel Keen <keendaniel01@gmail.com>
2018-07-01 13:29:32 +0100

    Setup Apollo

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink, InMemoryCache, ApolloClient } from 'apollo-boost';
 
 import Navigator from './components/Navigator';
+
+import GQL_URI from'./config/keys.js';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: GQL_URI
+  }),
+  cache: new InMemoryCache()
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <Navigator />
+      <ApolloProvider client={client}>
+        <Navigator />
+      </ApolloProvider>
     );
   }
 }
--- __tests__/user_auth_tests/CreateUser.test.js
+++ __tests__/user_auth_tests/CreateUser.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CreateUser from '../../components/user/CreateUser';
 
-const props = { type: 'Register' }
-const createUser = shallow(<CreateUser {...props} />);
+const createUser = shallow(<CreateUser />);
 
 it('renders without crashing', () => {
   expect(createUser).toMatchSnapshot();
